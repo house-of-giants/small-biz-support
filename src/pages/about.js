4f0877b183c33957d80860c6f2c2d65b760d3c3c
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-03-26 16:38:10 -0600

    Remove extra $

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ export default ({ data }) => (
 	<Layout>
 		<Head title={`${data.site.siteMetadata.city} Small Biz Support`} titleTemplate="%s" />
 		<StyledContent>
-			<p>Help support ${data.site.siteMetadata.city}'s small businesses by ordering from their online stores, online delivery services, or by calling in an order to take out during these strange times. Doing what we can to help the vibrant community we have thrive, even during times of uncertanty, will help us to maintain what makes our city great. Thank you for your support!</p>
+			<p>Help support {data.site.siteMetadata.city}'s small businesses by ordering from their online stores, online delivery services, or by calling in an order to take out during these strange times. Doing what we can to help the vibrant community we have thrive, even during times of uncertanty, will help us to maintain what makes our city great. Thank you for your support!</p>
 		</StyledContent>
 	</Layout>
 )
